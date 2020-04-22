b83cc0c57b6c5721f6f3ff0b07cc4b3191b8c91e
Gert-Jan <gertjanboth@gmail.cmo>
2020-04-22 11:51:33 +0200

    Library supports deriv and poly args; decorator now only promotes arrays

--- src/phimal_utilities/data/base.py
+++ src/phimal_utilities/data/base.py
@@ -1,11 +1,12 @@
 import torch
+from numpy import ndarray
 
 
 def pytorch_func(function):
     '''Decorator to automatically transform arrays to tensors and back'''
     def wrapper(self, *args, **kwargs):
-        torch_args = [torch.tensor(arg, requires_grad=True, dtype=torch.float64) for arg in args]
-        torch_kwargs = {key: torch.tensor(kwarg, requires_grad=True, dtype=torch.float64) for key, kwarg in kwargs.items()}
+        torch_args = [torch.tensor(arg, requires_grad=True, dtype=torch.float64) for arg in args if type(arg) is ndarray]
+        torch_kwargs = {key: torch.tensor(kwarg, requires_grad=True, dtype=torch.float64) for key, kwarg in kwargs.items() if type(kwarg) is ndarray}
         result = function(self, *torch_args, **torch_kwargs)
         return result.detach().numpy()
     return wrapper
@@ -29,16 +30,28 @@ class Dataset:
         return u_t
 
     @pytorch_func
-    def library(self, x, t):
+    def library(self, x, t, poly_order=2, deriv_order=2):
         ''' Returns library with 3rd order derivs and 2nd order polynomial'''
         assert ((x.shape[1] == 1) & (t.shape[1] == 1)), 'x and t should have shape (n_samples x 1)'
 
         u = self.solution(x, t, **self.parameters)
-        u_x = torch.autograd.grad(u, x, torch.ones_like(u), create_graph=True)[0]
-        u_xx = torch.autograd.grad(u_x, x, torch.ones_like(u_x), create_graph=True)[0]
-        u_xxx = torch.autograd.grad(u_xx, x, torch.ones_like(u_xx))[0]
 
-        derivs = torch.cat([torch.ones_like(u), u_x, u_xx, u_xxx], dim=1)
-        theta = torch.cat([derivs, u * derivs, u**2 * derivs], dim=1)
+        # Polynomial part
+        poly_library = torch.ones_like(u)
+        for order in torch.arange(1, poly_order+1):
+            poly_library = torch.cat((poly_library, poly_library[:, order-1:order] * u), dim=1)
 
+        # derivative part
+        if deriv_order == 0:
+            deriv_library = torch.ones_like(u)
+        else:
+            du = torch.autograd.grad(u, x, torch.ones_like(u), create_graph=True)[0]
+            deriv_library = torch.cat((torch.ones_like(u), du), dim=1)
+            if deriv_order > 1:
+                for order in torch.arange(1, deriv_order):
+                    du = torch.autograd.grad(deriv_library[:, order:order+1], x, grad_outputs=torch.ones_like(u), create_graph=True)[0]
+                    deriv_library = torch.cat((deriv_library, du), dim=1)
+
+        # Making library
+        theta = torch.matmul(poly_library[:, :, None], deriv_library[:, None, :]).reshape(u.shape[0], -1)
         return theta
--- tests/testing_torch.py
+++ tests/testing_torch.py
@@ -1,8 +1,7 @@
 import numpy as np
 from phimal_utilities.data import Dataset
 from phimal_utilities.data.diffusion import DiffusionGaussian
-from phimal_utilities.data.burgers import BurgersDelta, BurgersCos, BurgersSawtooth, BurgersDiscontinuous
-
+from phimal_utilities.data.burgers import BurgersDelta, BurgersCos, BurgersSawtooth
 
 x = np.linspace(0, 2*np.pi, 50)
 t = np.linspace(1e-4, 0.5, 20)
@@ -12,7 +11,6 @@ x_grid, t_grid = np.meshgrid(x, t)
 dataset = Dataset(BurgersDelta, v=0.1, A=1.0)
 dataset = Dataset(BurgersCos, v=0.1, a=0.1, b=0.1, k=2)
 dataset = Dataset(BurgersSawtooth, v=0.1)
-dataset = Dataset(BurgersDiscontinuous, v=1.0, a=0.5, b=0.1)
 
 
 dataset.generate_solution(x_grid, t_grid).shape
@@ -20,10 +18,10 @@ dataset.parameters
 
 dataset.time_deriv(x_grid, t_grid).shape
 
-theta = dataset.library(x_grid.reshape(-1, 1), t_grid.reshape(-1, 1))
+theta = dataset.library(x_grid.reshape(-1, 1), t_grid.reshape(-1, 1), poly_order=3, deriv_order=2)
 dt = dataset.time_deriv(x_grid.reshape(-1, 1), t_grid.reshape(-1, 1))
 
-
+theta.shape
 np.linalg.lstsq(theta, dt, rcond=None)[0]
 
 
@@ -37,3 +35,8 @@ y_train, X_train = dataset.generate_deepmod_dataset(x, t, noise=0.05, n_samples=
 
 from phimal_utilities.tensorboard import load_tensorboard
 df = load_tensorboard(path)
+
+
+import torch
+
+torch.arange(0, 1)

2aef4147ea416d64ba1870aeaf28a5b30cf061f1
Gert-Jan <gertjanboth@gmail.cmo>
2020-07-23 11:45:02 +0200

    load_tensorboard can work with new tensorboard files

--- src/phimal_utilities/analysis/tensorboard/load_tensorboard.py
+++ src/phimal_utilities/analysis/tensorboard/load_tensorboard.py
@@ -1,26 +1,30 @@
 import pandas as pd
-import numpy as np
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
-from os import listdir
+import os
 
 
 def load_tensorboard(path):
     '''Function to load tensorboard file from a folder.
     Assumes one file per folder!'''
 
-    event_file = next(filter(lambda filename: filename[:6] == 'events', listdir(path)))
-    summary_iterator = EventAccumulator(str(path + event_file)).Reload()
+    event_paths = [file for file in os.walk(path, topdown=True) if file[2][0][:len('events')] == 'events']
 
-    tags = summary_iterator.Tags()['scalars']
-    steps = [[event.step for event in summary_iterator.Scalars(tag)] for tag in tags]
-    data = [[event.value for event in summary_iterator.Scalars(tag)] for tag in tags]
-    
-    # Creating dataframe: we have missing values due to coefficients being deleted, 
-    #so we have to do this column by column
-    
     df = pd.DataFrame()
-    for idx, tag in enumerate(tags):
-        df[tag] = pd.Series(index=steps[idx], data=data[idx]) 
-    df[df.isna()] = 0.0 # if they're deleted, they're zero.
+    steps = None  # steps are the same for all files
+
+    for path in event_paths:
+        summary_iterator = EventAccumulator(os.path.join(path[0], path[2][0])).Reload()
+        tags = summary_iterator.Tags()['scalars']
+        data = [[event.value for event in summary_iterator.Scalars(tag)] for tag in tags]
+        if steps is None:
+            steps = [event.step for event in summary_iterator.Scalars(tags[0])]
+
+        # Adding to dataframe
+        tags = [tag.replace('/', '_') for tag in tags]  # for name consistency
+        if len(path[1]) == 0:  # if its the lowest level, use directory name as tag
+            tags = [path[0].split('/')[-1]]
+
+        for idx, tag in enumerate(tags):
+            df[tag] = data[idx]
 
     return df
